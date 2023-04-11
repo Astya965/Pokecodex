2481b6a24e0b7726f10ec1695e7daeb77e56f3a9
astya965 <astya965@gmail.com>
2023-04-11 21:30:24 +0200

    feat: Add not found page

---
+++ src/pages/notfound/NotFoundPage.tsx
@@ -0,0 +1,14 @@
+import Title from 'antd/es/typography/Title';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './NotFoundPage.scss';
+
+const NotFoundPage = () => (
+  <div className='errorPage'>
+    <h2 className='errorNumber'>404</h2>
+    <h3 className='errorInfo'>Page not found</h3>
+    <Link className='linkToHome' to={'/'}>Go to main page</Link>
+  </div>
+);
+
+export default NotFoundPage;
