13dd426dce894107b08c556fcd8d20022dc5a2f0
astya965 <astya965@gmail.com>
2023-04-12 15:56:19 +0200

    feat: Add tests for shared utils

---
+++ src/shared/tests/format.test.ts
@@ -0,0 +1,20 @@
+import { formatName } from '../utils/format';
+
+const name1 = 'ann';
+const name2 = 'ann-maxwell';
+const name3 = '';
+
+const result1 = 'Ann';
+const result2 = 'Ann Maxwell';
+
+test('ann => Ann', () => {
+  expect(formatName(name1)).toEqual(result1);
+});
+
+test('ann-maxwell => Ann Maxwell', () => {
+  expect(formatName(name2)).toEqual(result2);
+});
+
+test(' => ', () => {
+  expect(formatName(name3)).toEqual(name3);
+});
