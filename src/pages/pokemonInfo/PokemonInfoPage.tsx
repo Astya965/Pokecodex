e36b988b94b359f87840e21ffb67be38e9e35154
astya965 <astya965@gmail.com>
2023-04-12 14:20:59 +0200

    feat: Add Pokemon Info page

---
+++ src/pages/pokemonInfo/PokemonInfoPage.tsx
@@ -0,0 +1,88 @@
+import { gql, useLazyQuery } from '@apollo/client';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import PokemonInfo from '../../features/pokemonInfo';
+import { TPokemonFullInfo } from '../../shared/types/formatedPokemon';
+import { formatPokemonData } from './utils/pokemon';
+
+const PokemonInfoPage = () => {
+  const { id } = useParams();
+
+  const GET_POKEMON_BY_ID = gql`
+    query {
+      pokemons: pokemon_v2_pokemon(where: { id: { _eq: ${id} } }) {
+        id
+        name
+        types: pokemon_v2_pokemontypes {
+          type: pokemon_v2_type {
+            name
+          }
+        }
+        stats: pokemon_v2_pokemonstats {
+          base_stat
+          statName: pokemon_v2_stat {
+            name
+          }
+        }
+        abilities: pokemon_v2_pokemonabilities {
+          ability: pokemon_v2_ability {
+            name
+            text: pokemon_v2_abilityeffecttexts(
+              where: { language_id: { _eq: 9 } }
+            ) {
+              effect
+            }
+          }
+        }
+        specy: pokemon_v2_pokemonspecy {
+          base_happiness
+          capture_rate
+          is_baby
+          is_legendary
+          is_mythical
+          color: pokemon_v2_pokemoncolor {
+            name
+          }
+          habitat: pokemon_v2_pokemonhabitat {
+            name
+          }
+          shape: pokemon_v2_pokemonshape {
+            name
+          }
+          evolution: pokemon_v2_evolutionchain {
+            form: pokemon_v2_pokemonspecies(order_by: {evolves_from_species_id: asc_nulls_first}) {
+              name
+              id
+            }
+          }
+        }
+        weight
+        height
+      }
+    }
+  `;
+
+  const [queryGetPokemon, { data, loading, error }] =
+    useLazyQuery(GET_POKEMON_BY_ID);
+  const [pokemon, setPokemon] = useState<TPokemonFullInfo | null>(null);
+
+  useEffect(() => {
+    queryGetPokemon();
+  }, []);
+
+  useEffect(() => {
+    if (data?.pokemons?.length > 0 && data.pokemons[0]) {
+      setPokemon(formatPokemonData(data.pokemons[0]));
+    }
+  }, [data]);
+
+  return (
+    pokemon && (
+      <>
+        <PokemonInfo pokemon={pokemon} />
+      </>
+    )
+  );
+};
+
+export default PokemonInfoPage;
---
+++ src/pages/pokemonInfo/utils/pokemon.ts
@@ -0,0 +1,37 @@
+import { TPokemonFullInfo } from '../../../shared/types/formatedPokemon';
+import { TRawPokemonFullData } from '../../../shared/types/rawPokemonData';
+import { formatName } from '../../../shared/utils/format';
+
+export const formatPokemonData = (
+  pokemon: TRawPokemonFullData,
+): TPokemonFullInfo => {
+  return {
+    id: pokemon.id,
+    name: formatName(pokemon.name),
+    types: pokemon.types.map((item) => item.type.name),
+    stats: pokemon.stats.reduce((acc, item) => {
+      acc.set(item.statName.name, item.base_stat);
+      return acc;
+    }, new Map<string, number>()),
+    evolution: pokemon.specy.evolution.form.map((form) => ({
+      name: formatName(form.name),
+      id: form.id,
+    })),
+    abilities: pokemon.abilities.map((info) => ({
+      name: formatName(info.ability.name),
+      text: info.ability.text[0].effect,
+    })),
+    additionalInfo: {
+      height: pokemon.height,
+      weight: pokemon.weight,
+      color: pokemon.specy.color.name,
+      habitat: pokemon.specy.habitat.name,
+      shape: pokemon.specy.shape.name,
+      baseHappiness: pokemon.specy.base_happiness,
+      captureRate: pokemon.specy.capture_rate,
+      isBaby: pokemon.specy.is_baby,
+      isLegendary: pokemon.specy.is_legendary,
+      isMythical: pokemon.specy.is_mythical,
+    },
+  };
+};
