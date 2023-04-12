a4f730b6a33ea142e6e3998c8dc81d07cb3edd4c
astya965 <astya965@gmail.com>
2023-04-12 18:07:30 +0200

    fix: Fix pokemon info formatter

--- src/pages/pokemonInfo/utils/pokemon.ts
+++ src/pages/pokemonInfo/utils/pokemon.ts
@@ -8,30 +8,33 @@ export const formatPokemonData = (
   return {
     id: pokemon.id,
     name: formatName(pokemon.name),
-    types: pokemon.types.map((item) => item.type.name),
-    stats: pokemon.stats.reduce((acc, item) => {
+    types: pokemon.types?.map((item) => item.type.name),
+    stats: pokemon.stats?.reduce((acc, item) => {
       acc.set(item.statName.name, item.base_stat);
       return acc;
     }, new Map<string, number>()),
-    evolution: pokemon.specy.evolution.form.map((form) => ({
+    evolution: pokemon.specy?.evolution?.form?.map((form) => ({
       name: formatName(form.name),
       id: form.id,
     })),
-    abilities: pokemon.abilities.map((info) => ({
-      name: formatName(info.ability.name),
-      text: info.ability.text[0].effect,
+    abilities: pokemon.abilities?.map((info) => ({
+      name: formatName(info?.ability?.name),
+      text:
+        info.ability?.text?.length > 0
+          ? info.ability?.text[0].effect
+          : 'Without description',
     })),
     additionalInfo: {
       height: pokemon.height,
       weight: pokemon.weight,
-      color: pokemon.specy.color.name,
-      habitat: pokemon.specy.habitat.name,
-      shape: pokemon.specy.shape.name,
-      baseHappiness: pokemon.specy.base_happiness,
-      captureRate: pokemon.specy.capture_rate,
-      isBaby: pokemon.specy.is_baby,
-      isLegendary: pokemon.specy.is_legendary,
-      isMythical: pokemon.specy.is_mythical,
+      color: pokemon.specy?.color?.name,
+      habitat: pokemon.specy?.habitat?.name,
+      shape: pokemon.specy?.shape?.name,
+      baseHappiness: pokemon.specy?.base_happiness,
+      captureRate: pokemon.specy?.capture_rate,
+      isBaby: pokemon.specy?.is_baby,
+      isLegendary: pokemon.specy?.is_legendary,
+      isMythical: pokemon.specy?.is_mythical,
     },
   };
 };
